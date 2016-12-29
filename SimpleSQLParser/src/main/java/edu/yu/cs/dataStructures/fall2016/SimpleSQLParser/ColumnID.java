9ccba3e80f43f130482c6e59d277d6012f34e55d
diament <diament@yu.edu>
2016-12-29 12:37:08 -0500

    added equals and hashCode to ColumnID

--- SimpleSQLParser/src/main/java/edu/yu/cs/dataStructures/fall2016/SimpleSQLParser/ColumnID.java
+++ SimpleSQLParser/src/main/java/edu/yu/cs/dataStructures/fall2016/SimpleSQLParser/ColumnID.java
@@ -12,26 +12,58 @@ public class ColumnID
     
     public ColumnID(String columnName, String tableName)
     {
-	this.columnName = columnName;
-	this.tableName = tableName;
+    	this.columnName = columnName;
+    	this.tableName = tableName;
     }   
     public String getColumnName()
     {
-	return this.columnName;
+    	return this.columnName;
     }
     public String getTableName()
     {
-	return this.tableName;
+    	return this.tableName;
     }
     /**
      * @return the table name, followed by a period, followed by the column name
      */
     public String toString()
     {
-	if(this.tableName != null)
-	{
-	    return this.tableName + "." + this.columnName;
-	}
-	return this.columnName;
+    	if(this.tableName != null)
+    	{
+    		return this.tableName + "." + this.columnName;
+    	}
+    	return this.columnName;
     }
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((columnName == null) ? 0 : columnName.hashCode());
+		result = prime * result + ((tableName == null) ? 0 : tableName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ColumnID other = (ColumnID) obj;
+		if (columnName == null) {
+			if (other.columnName != null)
+				return false;
+		} else if (!columnName.equals(other.columnName))
+			return false;
+		if (tableName == null) {
+			if (other.tableName != null)
+				return false;
+		} else if (!tableName.equals(other.tableName))
+			return false;
+		return true;
+	}
+    
 }
